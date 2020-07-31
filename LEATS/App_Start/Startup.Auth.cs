e8346b8d881cfd6ab7546a05908870ffad7fce9d
Ndumiso <217050448@stu.ukzn.ac.za>
2020-07-31 20:41:22 +0200

    Security Fixed

--- LEATS/App_Start/Startup.Auth.cs
+++ LEATS/App_Start/Startup.Auth.cs
@@ -74,9 +74,9 @@ namespace LEATS
             //   consumerKey: "XXX",
             //   consumerSecret: "XXX");
 
-            app.UseFacebookAuthentication(
-               appId: "3145935258819254",
-               appSecret: "a4769b9c8322043fc577abf60db36851");
+            //app.UseFacebookAuthentication(
+            //   appId: "XXXX",
+            //   appSecret: "XXX");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
