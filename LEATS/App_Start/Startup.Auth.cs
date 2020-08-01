29db42a9f7fc1ed492e25b187184f573a540f8c6
Khayeh <khayeh253@gmail.com>
2020-08-01 11:26:11 +0200

    Revert to no themes

--- LEATS/App_Start/Startup.Auth.cs
+++ LEATS/App_Start/Startup.Auth.cs
@@ -6,9 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using LEATS.Models;
-using Microsoft.Owin.Security.Twitter;
-using Microsoft.Owin.Security;
-//Comment
+
 namespace LEATS
 {
     public partial class Startup
@@ -52,31 +50,13 @@ namespace LEATS
             //    clientId: "",
             //    clientSecret: "");
 
-            //app.UseTwitterAuthentication(new TwitterAuthenticationOptions
-            //{
-            //    ConsumerKey = "XXXX",
-            //    ConsumerSecret = "XXXX",
-
-            //    BackchannelCertificateValidator = new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
-            //    {
-            //        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
-            //        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
-            //        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
-            //        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
-            //        "‎add53f6680fe66e383cbac3e60922e3b4c412bed", // Symantec Class 3 EV SSL CA - G3
-            //        "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
-            //        "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
-            //        "B13EC36903F8BF4701D498261A0802EF63642BC3" // DigiCert High Assurance EV Root CA
-            //    })
-            //});
-
             //app.UseTwitterAuthentication(
-            //   consumerKey: "XXX",
-            //   consumerSecret: "XXX");
+            //   consumerKey: "",
+            //   consumerSecret: "");
 
             //app.UseFacebookAuthentication(
-            //   appId: "XXXX",
-            //   appSecret: "XXX");
+            //   appId: "",
+            //   appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
